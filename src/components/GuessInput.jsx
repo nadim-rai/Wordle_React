4edfa53e6d591e698716ca116f5fbd385290e7f7
Nadim Rai <nadim_rai@yahoo.com>
2026-03-04 21:40:58 +0800

    Guess Input component created

---
+++ src/components/GuessInput.jsx
@@ -0,0 +1,9 @@
+function GuessInput(){
+    return(
+        <form className="guess-input-wrapper">
+            <label htmlFor="guess-input">Enger guess:</label>
+            <input type="text" id="guess-input" />
+        </form>
+    )
+}
+export default GuessInput;
