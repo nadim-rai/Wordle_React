ce48e2ebe6ef02571384e31ad2e96cb3ca08435c
Nadim Rai <nadim_rai@yahoo.com>
2026-03-04 22:30:05 +0800

    forms able to validate input data

--- src/components/GuessInput.jsx
+++ src/components/GuessInput.jsx
@@ -1,8 +1,29 @@
+import React from "react";
+
 function GuessInput(){
+    const [guess, setGuess] = React.useState('');
+    function handleSubmit(event){
+        event.preventDefault();
+        console.log({guess})
+        setGuess('');
+    }
     return(
-        <form className="guess-input-wrapper">
+        <form onSubmit={handleSubmit}className="guess-input-wrapper">
             <label htmlFor="guess-input">Enter guess:</label>
-            <input type="text" id="guess-input" />
+            <input 
+                required
+                minLength={5}
+                maxLength={5}
+                pattern="[a-zA-Z]{5}"
+                title="5 letter word"
+                type="text" 
+                id="guess-input" 
+                value={guess}
+                onChange={(event) =>{
+                    const nextGuess = event.target.value.toUpperCase();
+                    setGuess(nextGuess)
+                }}
+            />
         </form>
     )
 }
