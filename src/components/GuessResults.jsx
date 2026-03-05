d3ae9f8d3d86c283c3897e7bf96b3d4a933944a9
Nadim Rai <nadim_rai@yahoo.com>
2026-03-05 23:58:27 +0800

    created GuessResults file to display Guess Inputs

---
+++ src/components/GuessResults.jsx
@@ -0,0 +1,14 @@
+
+
+function GuessResults({guesses}){
+    return(
+        <div className="guess-results">
+            {guesses.map((guess, index) => (
+                 <p key={index} className="guess">
+                    {guess}
+                </p>
+            ))}
+        </div>
+    )
+}
+export default GuessResults;
