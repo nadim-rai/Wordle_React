9ec03df4f54d50a4d0f4e6b6de18613e9a21a5ae
Nadim Rai <nadim_rai@yahoo.com>
2026-03-08 23:26:09 +0800

    keep track of game status

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { sample } from "../utils";
 import { WORDS} from '../data';
+import { NUM_OF_GUESSES_ALLOWED } from "../constants";
 
 import GuessInput from "./GuessInput";
 import GuessResults from "./GuessResults";
@@ -11,12 +12,21 @@ const answer = sample(WORDS);
 console.log({answer})
 
 function Game(){
+    const [gameStatus, setGameStatus] = React.useState('running');
+
     const [guesses, setGuesses] = React.useState([])
     function handleSubmitGuess(tentativeGuess){
-        setGuesses([...guesses, tentativeGuess])
+        const nextGuesses = [...guesses, tentativeGuess];
+        setGuesses(nextGuesses);
+        if(tentativeGuess.toUpperCase() === answer){
+            setGameStatus('won')
+        }else if(nextGuesses.length >= NUM_OF_GUESSES_ALLOWED){
+            setGameStatus('lost');
+        }
     }
     return(
         <>
+            {gameStatus}
             <GuessResults guesses={guesses} answer={answer}/>
             <GuessInput handleSubmitGuess={handleSubmitGuess} />
         </>
